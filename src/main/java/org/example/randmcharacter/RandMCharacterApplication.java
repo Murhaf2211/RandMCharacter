91d8953885fb004efd5eb4d4051f1d40f46080c5
murhaf2211 <orfali.murhaf@web.de>
2024-09-12 11:02:38 +0200

    Initial commit

---
+++ src/main/java/org/example/randmcharacter/RandMCharacterApplication.java
@@ -0,0 +1,13 @@
+package org.example.randmcharacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandMCharacterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandMCharacterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/randmcharacter/RandMCharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.randmcharacter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandMCharacterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
